cfc77c8c5933fce36e7f32730328ace975b1aa57
lockemind <pedr.amaral@gmail.com>
2015-04-27 23:36:01 +0100

    added about page

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -10,5 +10,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
-
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
 end
